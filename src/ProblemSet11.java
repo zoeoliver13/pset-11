ef39b0aa4f399abee1748331fd1a44307780fa68
Ryan Wilson <ryan.wilson@empiriolabs.io>
2021-02-09 11:53:28 -0500

    initial commit

---
+++ src/ProblemSet11.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+
+public class ProblemSet11 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public ArrayList<String> fizzBuzz(int start, int end) {
+
+    }
+
+    public int maxSpan(ArrayList<Integer> numbers) {
+
+    }
+
+    public ArrayList<Integer> fix34(ArrayList<Integer> numbers) {
+
+    }
+
+    public ArrayList<Integer> fix45(ArrayList<Integer> numbers) {
+
+    }
+
+    public boolean canBalance(ArrayList<Integer> numbers) {
+
+    }
+
+    public boolean linearIn(ArrayList<Integer> outer, ArrayList<Integer> inner) {
+
+    }
+
+    public ArrayList<Integer> squareUp(int n) {
+
+    }
+
+    public ArrayList<Integer> seriesUp(int n) {
+
+    }
+
+    public int maxMirror(ArrayList<Integer> numbers) {
+
+    }
+
+    public int countClumps(ArrayList<Integer> numbers) {
+
+    }
+}
